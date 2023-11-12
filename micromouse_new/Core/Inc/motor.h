70105b5ec0827425284763d93d606d7f9b9f7c2e
Alex Naehu <naehuthesnail@gmail.com>
2023-11-12 11:34:56 -0800

    created motor files

---
+++ micromouse_new/Core/Inc/motor.h
@@ -0,0 +1,15 @@
+/*
+ * motor.h
+ *
+ *  Created on: Nov 11, 2023
+ *      Author: lethalpanda13
+ */
+
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+
+#include "main.h"
+
+
+#endif /* INC_MOTOR_H_ */
---
+++ micromouse_new/Core/Src/motor.cpp
@@ -0,0 +1,10 @@
+/*
+ * motor.cpp
+ *
+ *  Created on: Nov 11, 2023
+ *      Author: lethalpanda13
+ */
+
+#include "motor.h"
+
+
